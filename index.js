fb76220c8d03255153a7491bc085cb484f48062a
Chau Tran <cctran96@gmail.com>
2021-03-25 17:56:41 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector('main')
+main.remove()
+
+const header = document.createElement('h1')
+document.body.appendChild(header)
+header.id = 'victory'
+const newHeader = document.getElementById('victory')
+newHeader.innerHTML = 'YOUR-NAME is the champion'
